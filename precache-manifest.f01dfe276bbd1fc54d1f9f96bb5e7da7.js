820adaf7f03f192671b484389d1dc166a74e1cde
Ruoyi Qiu <qry@RuoyideMacBook-Pro.local>
2020-07-09 00:47:36 +1000

    create portfolio

---
+++ precache-manifest.f01dfe276bbd1fc54d1f9f96bb5e7da7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9f45b6bad74706a7588e326c765657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc95a857d825c367ab0f",
+    "url": "/static/css/2.8d7803e9.chunk.css"
+  },
+  {
+    "revision": "b617bb1df9f88b9f1c97",
+    "url": "/static/css/main.52c580c4.chunk.css"
+  },
+  {
+    "revision": "dc95a857d825c367ab0f",
+    "url": "/static/js/2.61218b5a.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.61218b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b617bb1df9f88b9f1c97",
+    "url": "/static/js/main.5ca32c9b.chunk.js"
+  },
+  {
+    "revision": "3dbe7852a01fcf3a7862",
+    "url": "/static/js/runtime-main.2128c43b.js"
+  }
+]);
